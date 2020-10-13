fa29e21ddc7dc46dfeedfd7147891108ae2efb62
Ricky Sullivan <rickyplaysdrums@gmail.com>
2020-10-13 17:56:32 -0500

    dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var createTaskHandler = function(){
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task.";
-   tasksToDoE1.appendChild(listItemE1);
+    tasksToDoE1.appendChild(listItemE1);
 }
 
 buttonE1.addEventListener("click", createTaskHandler);
